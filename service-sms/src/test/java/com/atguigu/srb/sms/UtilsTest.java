f2eb78e02b84997473adebaa1bf9a7de91bade17
cjiankai <cjiankai@sina.com>
2021-11-08 17:14:03 +0800

    changed: 自定义配置及单元测试

---
+++ service-sms/src/test/java/com/atguigu/srb/sms/UtilsTest.java
@@ -0,0 +1,24 @@
+package com.atguigu.srb.sms;
+
+import com.atguigu.srb.sms.util.SmsProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author jiankai
+ * @description:
+ * @date 2021-11-08 17:10
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UtilsTest {
+
+    @Test
+    public void testProperties() {
+        System.out.println(SmsProperties.KEY_ID);
+        System.out.println(SmsProperties.KEY_SECRET);
+        System.out.println(SmsProperties.REGION_Id);
+    }
+}
